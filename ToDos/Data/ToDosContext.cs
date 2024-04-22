4f37cc7e49078a249ca56fcff5be36930967d102
TkDevk <duvanuriel@hotmail.com>
2024-04-22 14:08:17 -0500

    Add project files.

---
+++ ToDos/Data/ToDosContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDos.Models;
+
+namespace ToDos.Data
+{
+    //DbContext is a class provided by Entity Framework Core for managing database connections and performing CRUD operations.
+    public class ToDosContext : DbContext
+    {
+        //Contructor's ToDosContext
+        public ToDosContext (DbContextOptions<ToDosContext> options)
+            //It takes an instance of DbContextOptions<ToDosContext>
+            : base(options)
+        {
+        }
+        //DbSet<T> is a property of DbContext
+        //ToDo.Models.ToDo specifies the type of entity that will be stored in this DbSet
+        public DbSet<ToDos.Models.ToDo> ToDo { get; set; } = default!;
+    }
+}
---
+++ ToDos/Models/ToDo.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDos.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public string Description { get; set; }
+    }
+}
